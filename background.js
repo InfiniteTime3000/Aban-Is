57ecaf9da01e3b7f397f5a82cebf4d35b1414d54
InfiniteTime@ya <syedaban3000@yahoo.com>
2024-11-26 13:13:22 +0530

    bug fix with bg.js

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 window.setInterval(() => {
     if ((new Date().getTime() - localStorage.getItem("btnClickedAt"))/60000 >= 5) {
         document.getElementById("heartBtn").removeAttribute("disabled")
+    } else {
+        document.getElementById("heartBtn").setAttribute("disabled", true)
     }
 }, 1000)
