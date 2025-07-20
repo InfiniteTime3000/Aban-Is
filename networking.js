72dc1458d9b927567ddbaaf67223e1466a37aed6
infinitetime3000 <syedaban3000@yahoo.com>
2025-07-20 23:20:45 +0530

    Added fetch get with timeout

--- networking.js
+++ networking.js
@@ -3,11 +3,22 @@ fetch('https://intimate-crappie-frank.ngrok-free.app/', {
 }).then(response => response.json()).then(data => {
     document.getElementsByTagName('i')[0].innerText = data.msg
 }).catch(() => {
-    fetch('https://aban-is-server.onrender.com/', {
+    fetchWithTimeout('https://aban-is-server.onrender.com/', {
         method: 'GET'
     }).then(response => response.json()).then(data => {
         document.getElementsByTagName('i')[0].innerText = data.msg
     }).catch(() => {
-        document.getElementsByTagName('i')[0].innerText = "sleeping or his laptop is off (server is down)"
+        document.getElementsByTagName('i')[0].innerText = "sleeping or his laptop is off"
     })
 })
+
+function fetchWithTimeout(url, timeout = 3000) {
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const timer = setTimeout(() => {
+    controller.abort();
+  }, timeout);
+
+  return fetch(url, { signal }).finally(() => clearTimeout(timer));
+}
